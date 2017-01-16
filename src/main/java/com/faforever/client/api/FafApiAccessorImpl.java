beefdc8da63c75ab9e0609df8e0a58ae48963970
Alexander <alexander.von.trostorff@gmail.com>
2017-01-16 22:25:14 +0100

    deleted comment

--- src/main/java/com/faforever/client/api/FafApiAccessorImpl.java
+++ src/main/java/com/faforever/client/api/FafApiAccessorImpl.java
@@ -162,7 +162,6 @@ public class FafApiAccessorImpl implements FafApiAccessor {
   @Override
   public List<Clan> getClans() {
     logger.debug("Loading Clan");
-    //makes some error
     return getMany("/clans", Clan.class);
   }
 
